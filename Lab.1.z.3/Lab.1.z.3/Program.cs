80b88425d91a33508721c2136d5fcf715c4ac4aa
student <student@C246U44>
2021-02-19 10:50:28 +0300

    Задание 3

---
+++ Lab.1.z.3/Lab.1.z.3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Lab._1.z._3
+{
+  class Person
+  {
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+  }
+
+  class Program
+  {
+    static async Task Main(string[] args)
+    {
+      using (FileStream fs = new FileStream("user.json", FileMode.OpenOrCreate))
+      {
+        Person tom = new Person() { Name = "Anton", Age = 20 };
+        await JsonSerializer.SerializeAsync<Person>(fs, tom);
+        Console.WriteLine("Data has been saved to file");
+      }
+
+      // чтение данных
+      using (FileStream fs = new FileStream("user.json", FileMode.OpenOrCreate))
+      {
+        Person restoredPerson = await JsonSerializer.DeserializeAsync<Person>(fs);
+        Console.WriteLine($"Name: {restoredPerson.Name}  Age: {restoredPerson.Age}");
+      }
+
+    }
+  }
+
+}
